20dd97136607730d4234ab82196f47a87394e434
sudhanv09 <mystogun125@gmail.com>
2025-11-10 17:23:15 +0800

    redefine state

--- env.py
+++ env.py
@@ -1,3 +1,6 @@
 
 class World:
-    pass
+    def __init__(self):
+        self.current_epoch: int = 0
+        self.total_orders_available: int = 0
+        
--- state.py
+++ state.py
@@ -1,23 +1,38 @@
 
 class Company:
-    def __init__(self, total_drivers: int, seed_money: int):
-        self.total_drivers = total_drivers
+    def __init__(self, drivers: int, seed_money: int):
+        self.total_drivers = drivers
         self.seed_money = seed_money
         
-        self.profit: int = 0
-        self.driver_churn: float = 0.0
+        # Balance sheet
+        self.current_balance: float = seed_money
+        self.last_epoch_profit: float = 0.0
+        self.consecutive_loss_epochs: int = 0
 
-        self.avg_driver_earnings: float = None
-        self.avg_order_price: float = None
-        self.avg_eta: float = None
-        self.rejected_rate: float = None
+        # Driver metrics
+        self.active_drivers = drivers
+        self.driver_churn_rate: float = 0.0
+        self.new_signups: int = 0
+
+        # Operation Metrics
+        self.avg_driver_profit: float = 0.0
+        self.avg_order_price: float = 0.0
+        self.avg_completion_rate: float = 1.0
 
 
 class Driver:
-    def __init__(self):
-        self.profit: int = 0
-        self.satisfaction_score: float = 1.0
+    def __init__(self, driver_id: int):
+        self.id = driver_id
 
-        self.num_order_completed: int = 0
-        self.avg_idle_time: float = 0.0
-        self.avg_delivery_distance: float = 0.0
+        # Performance
+        self.monthly_profit: float = 0
+
+        self.num_orders_completed: int = 0
+        self.total_kms_driven: float = 0.0
+        self.cost_per_km: float = 0.3
+        
+        self.profit_threshold = 35_000
+        self.satisfaction_score = 0.8
+        self.is_active: bool = True
+
+    
